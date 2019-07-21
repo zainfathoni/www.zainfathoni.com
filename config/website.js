06e067ff691aa8594c106b7366dab75c8746b4ba
Zain Fathoni <zain.fathoni@gmail.com>
2019-07-21 23:17:18 +0800

    Whitelabel site metadata

--- config/website.js
+++ config/website.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteTitle: 'My Blog', // Navigation and Site Title
-  siteTitleAlt: 'The blog of an egghead creator', // Alternative Site title for SEO
-  siteTitleShort: 'Starter Blog', // short_name for manifest
-  siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
+  siteTitle: 'Zain Fathoni', // Navigation and Site Title
+  siteTitleAlt: "Zain Fathoni's personal website", // Alternative Site title for SEO
+  siteTitleShort: 'Zain', // short_name for manifest
+  siteUrl: 'https://www.zainfathoni.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'This is where they post things!',
-  author: 'Author', // Author for schemaORGJSONLD
-  organization: 'egghead.io LLC',
+  siteDescription: "Zain Fathoni's personal website",
+  author: 'Zain Fathoni', // Author for schemaORGJSONLD
+  organization: 'www.zainfathoni.com',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@eggheadio', // Twitter Username
-  ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
+  userTwitter: '@zainfathoni', // Twitter Username
+  ogSiteName: "Zain Fathoni's personal website", // Facebook Site Name
   ogLanguage: 'en_US',
   googleAnalyticsID: '',
 
@@ -21,8 +21,8 @@ module.exports = {
   backgroundColor: '#2b2e3c',
 
   // Social component
-  twitter: 'https://twitter.com/eggheadio/',
-  twitterHandle: '@eggheadio',
-  github: 'https://github.com/eggheadio/',
-  linkedin: '',
+  twitter: 'https://twitter.com/zainfathoni/',
+  twitterHandle: '@zainfathoni',
+  github: 'https://github.com/zainfathoni/',
+  linkedin: 'https://www.linkedin.com/in/zainfathoni',
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,7 @@ const Hero = () => {
             max-width: ${rhythm(15)};
           `}
         >
-          Your blog says the things you want to say.
+          Hi! I'm Zain Fathoni, an Indonesian Developer Living in Singapore.
         </h1>
       </Container>
       <div
